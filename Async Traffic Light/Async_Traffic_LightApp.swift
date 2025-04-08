eb0edd0c368672f9ef1c58fd4f7089786f86e2f6
marc4919 <marcgeek4919@gmail.com>
2025-04-08 19:25:19 +0200

    Initial Commit

---
+++ Async Traffic Light/Async_Traffic_LightApp.swift
@@ -0,0 +1,17 @@
+//
+//  Async_Traffic_LightApp.swift
+//  Async Traffic Light
+//
+//  Created by Marc Garcia Teodoro on 8/4/25.
+//
+
+import SwiftUI
+
+@main
+struct Async_Traffic_LightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
